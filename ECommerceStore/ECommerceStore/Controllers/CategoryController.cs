3fc374fb26a26a81037b389439e7902ced7df195
shailu <shailendrakumar8859684259@gmail.com>
2022-10-14 13:22:23 +0530

    ui Changes

--- ECommerceStore/ECommerceStore/Controllers/CategoryController.cs
+++ ECommerceStore/ECommerceStore/Controllers/CategoryController.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SharedModel.Models;
+
 
 namespace ECommerceStore.Controllers
 {
     public class CategoryController : Controller
     {
+        int i = 2;
         // GET: Category
-        public ActionResult SubCategory()
+        public ActionResult Category()
+        {
+            Category category = new Category();
+           
+            return View();
+        }
+       
+        [HttpPost]
+        public ActionResult SubCategory(Category cat)
         {
             return View();
         }
+
+
     }
 }
