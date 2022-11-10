07300fc32ec2b25be4e93938c951f340f5937607
Poonam Yadav <poonampcc94@gmail.com>
2022-11-10 11:46:27 +0530

    changes

---
+++ AdminApi/SharedModel/Models/CommonAPI.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Xml;
+
+namespace SharedModel.Models
+{
+    public class CommonAPI : ICommonAPI
+    {
+        public Response Get(string url)
+        {
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpWebRequest.ContentType = "text/json";
+            httpWebRequest.Method = "GET";
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            string responseText = string.Empty;
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream())) { responseText = streamReader.ReadToEnd(); }
+            Response responseResult = JsonConvert.DeserializeObject<Response>(responseText);
+            return responseResult;
+        }
+
+        public Response Post(string url, object data)
+        {
+            JsonSerializerSettings serializerSettings = new JsonSerializerSettings();
+            serializerSettings.Converters.Add(new DataTableConverter());
+            string jsonString = JsonConvert.SerializeObject(data, Newtonsoft.Json.Formatting.None, serializerSettings);
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            { streamWriter.Write(jsonString); streamWriter.Flush(); streamWriter.Close(); }
+            string responseText = string.Empty;
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream())) { responseText = streamReader.ReadToEnd(); }
+            Response responseResult = JsonConvert.DeserializeObject<Response>(responseText);
+            return responseResult;
+        }
+    }
+}
